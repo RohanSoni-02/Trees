1a52bd108f3611707d504b43355b937d74971cd2
RohanSoni-02 <soni.rohanchd@gmail.com>
2024-02-16 19:52:18 +1100

    added insert fn

--- Trees/main.cpp
+++ Trees/main.cpp
@@ -33,5 +33,35 @@ public:
     BinarySearchTree(){
         root = nullptr;
     }
+    
+    bool insert(int value){
+        Node* newNode = new Node(value);
+        if(root == nullptr){
+            root = newNode;
+            return true;
+        }
+        Node* temp = root;
+        while (true) {
+            if (newNode->value == temp->value) {
+                return false;
+            }
+            
+            if (newNode->value < temp->value) {
+                if (temp->left == nullptr) {
+                    temp->left = newNode;
+                    return true;
+                }
+                temp = temp->left;
+            }
+            else{
+                if (temp->right == nullptr) {
+                    temp->right = newNode;
+                    return true;
+                }
+                temp = temp->right;
+            }
+        }
+    }
+    
 };
 
